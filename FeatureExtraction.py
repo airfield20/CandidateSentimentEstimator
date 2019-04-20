5cb980c83e1c73ada100d49b014458e55d9ebf8d
Matthew Abraham <mattjay.abraham@gmail.com>
2019-04-20 15:33:24 -0400

    Update FeatureExtraction.py
    added slightly better stopwords list

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -2,13 +2,19 @@ import pandas as pd
 import numpy as np
 from csv import reader
 from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk.tokenize import word_tokenize
+from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, TweetTokenizer
 
+# custom stopwords #
+ENGLISH_CUSTOM = stop = set([word for word in ENGLISH_STOP_WORDS]).union(set(stopwords.words('English'))).union(set(['\\', ':' '/', '-', "'", '"', '.', ',', "i've", "i'm", "i'll", '&']))
 
 # finds the most asscoiated words with a particular emotion #
-def create_word_bags(documents, labels, classes, keep_highest=4):
-    vec = TfidfVectorizer(stop_words='english')
+def create_word_bags(documents, labels, classes, keep_highest=4, custom_tokenizer=None):
+    if custom_tokenizer:
+        vec = TfidfVectorizer(stop_words=ENGLISH_CUSTOM, tokenizer=custom_tokenizer)
+    else:
+        vec = TfidfVectorizer(stop_words=ENGLISH_CUSTOM)
 
     tfidf_whole = vec.fit_transform(documents)
     words = vec.get_feature_names()
@@ -29,11 +35,16 @@ def create_word_bags(documents, labels, classes, keep_highest=4):
         bags[cl] = set(bag.head(keep_highest)['word'])
 
     return bags
+
+
         
 if __name__ == '__main__':
     from TweetReader import read_tweets
     docs_df, label_df = read_tweets('2018-E-c-En-train.txt', sep_char='\t')
-    bags = create_word_bags(docs_df, label_df, np.unique(label_df.columns), 60)
+
+    token_gen = TweetTokenizer(reduce_len=True)
+    
+    bags = create_word_bags(docs_df, label_df, np.unique(label_df.columns), 60, custom_tokenizer=token_gen.tokenize)
 
     #extractor_out = open('Extraction.out', mode='w')
 
