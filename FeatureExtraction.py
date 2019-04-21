bacfc4c64adc34bad8fc1ab4b1f23f4b1be16cd4
Matthew Abraham <mattjay.abraham@gmail.com>
2019-04-20 21:46:22 -0400

    Finished Feature extractor
    Added ability to load past generated word bags and read tweets directly, rather than processing each time

--- FeatureExtraction.py
+++ FeatureExtraction.py
@@ -1,53 +1,141 @@
 import pandas as pd
 import numpy as np
-from csv import reader
+import string
+import pickle
+from csv import writer
+from io import StringIO
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import ENGLISH_STOP_WORDS
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize, TweetTokenizer
 
-# custom stopwords #
-ENGLISH_CUSTOM = stop = set([word for word in ENGLISH_STOP_WORDS]).union(set(stopwords.words('English'))).union(set(['\\', ':' '/', '-', "'", '"', '.', ',', "i've", "i'm", "i'll", '&']))
+class FeatureExtractor:
+    def __init__(self, documents, labels, tokenizer=None, stop_words=None, bag_file=None):
+        self.documents = documents
+        self.labels = labels
+        self.classes = labels.columns
+        
+        if not stop_words:
+            self.custom_stopwords = stop = set([word for word in ENGLISH_STOP_WORDS]).union(set(stopwords.words('English'))).union(set(['\\', ':' '/', '-', "'", '"', '.', ',', "i've", "i'm", "i'll", '&']))
+        else:
+            self.custom_stopwords = stop_words
 
+        self.tokenizer = word_tokenize if not tokenizer else tokenizer
+
+        if bag_file:
+            try:
+                with open(bag_file, 'rb') as file:
+                    self.bags = pickle.load(file)
+                    self.set_count = len(self.bags[self.classes[0]])
+            except IOError:
+                print("An error occured while trying to read file.")
+                self.bags = None
+        else:
+            self.bags = None
+    
 # finds the most asscoiated words with a particular emotion #
-def create_word_bags(documents, labels, classes, keep_highest=4, custom_tokenizer=None):
-    if custom_tokenizer:
-        vec = TfidfVectorizer(stop_words=ENGLISH_CUSTOM, tokenizer=custom_tokenizer)
-    else:
-        vec = TfidfVectorizer(stop_words=ENGLISH_CUSTOM)
+    def create_word_bags(self, keep_highest=20):
+        self.set_count = keep_highest
+        vec = TfidfVectorizer(stop_words=self.custom_stopwords, tokenizer=self.tokenizer)
 
-    tfidf_whole = vec.fit_transform(documents)
-    words = vec.get_feature_names()
+        tfidf_whole = vec.fit_transform(self.documents)
+        words = vec.get_feature_names()
 
-    bags = {}
+        self.bags = {}
 
-    for cl in classes:
-        label = labels[cl]
-        samples = tfidf_whole[np.where(label)].toarray()
+        for cl in self.classes:
+            label = self.labels[cl]
+            samples = tfidf_whole[np.where(label)].toarray()
 
-        sample_means = np.mean(samples, axis=0)
-        sorted_means_idx = np.argsort(sample_means)[::-1]
-        sample_means = [(words[i], sample_means[i]) for i in sorted_means_idx]
+            sample_means = np.mean(samples, axis=0)
+            sorted_means_idx = np.argsort(sample_means)[::-1]
+            sample_means = [(words[i], sample_means[i]) for i in sorted_means_idx]
 
-        bag = pd.DataFrame(sample_means)
-        bag.columns = ['word', 'tfidf_score']
+            bag = pd.DataFrame(sample_means)
+            bag.columns = ['word', 'tfidf_score']
 
-        bags[cl] = set(bag.head(keep_highest)['word'])
+            self.bags[cl] = set(bag.head(keep_highest)['word'])
 
-    return bags
+    def save_word_bags(self):
+        with open('extracted-bags-count-' + str(self.set_count) + '.pickle', 'wb') as file:
+            pickle.dump(self.bags, file)
 
+    def feature_count_class_words(self):
+        assert self.bags, "No word bag found. Did you call create_word_bags() before this?"
+            
+        features_csv = StringIO()
+        feature_writer = writer(features_csv)
 
+        feature_writer.writerow(self.classes)
+        
+        for idx, document in self.documents.iteritems():
+            row = pd.Series(index=self.classes, dtype=np.int32).fillna(value=0)
+            tokens = self.tokenizer(document)
+            for token in tokens:
+                for cls in self.classes:
+                    if token in self.bags[cls]:
+                        row[cls] += 1
+
+            feature_writer.writerow(row.tolist())
+
+        features_csv.seek(0)
+
+        return pd.read_csv(features_csv, engine='c')
+
+# document length in words (basically, len(tokenizer(text)) #
+    def get_doc_len(self, text):
+        return len([word for word in self.tokenizer(text) if word not in string.punctuation])
+
+    def feature_doc_len(self):
+        return self.documents.apply(self.get_doc_len).to_frame(name='doc_len')
+
+# average word length #
+    def get_avg_word_len(self, text):
+        words = self.tokenizer(text)
+        return sum([len(word) for word in words]) / len(words)
+    
+    def feature_avg_word_len(self):
+        return self.documents.apply(self.get_avg_word_len).to_frame(name='avg_word_len')
+
+# frequency of upper, lower, and title cased words #
+    def get_upper_count(self, text):
+        return len([word for word in self.tokenizer(text) if word.isupper()])
+
+    def get_lower_count(self, text):
+        return len([word for word in self.tokenizer(text) if word.islower()])
+
+    def get_titled_count(self, text):
+        return len([word for word in self.tokenizer(text) if word.istitle()])
+
+    def feature_fraction_upper(self):
+        return self.documents.apply(lambda text: self.get_upper_count(text) / self.get_doc_len(text)).to_frame(name='fraction_upper')
+
+    def feature_fraction_lower(self):
+        return self.documents.apply(lambda text: self.get_lower_count(text) / self.get_doc_len(text)).to_frame(name='fraction_lower')
+
+    def feature_fraction_titled(self):
+        return self.documents.apply(lambda text: self.get_titled_count(text) / self.get_doc_len(text)).to_frame(name='fraction_titled')
         
 if __name__ == '__main__':
-    from TweetReader import read_tweets
-    docs_df, label_df = read_tweets('2018-E-c-En-train.txt', sep_char='\t')
+    from TweetReader import TweetReader
+
+    reader = TweetReader()
+    docs_df, label_df = reader.load_tweets('tweet_data/tweet_training.pickle')
 
     token_gen = TweetTokenizer(reduce_len=True)
+    extractor = FeatureExtractor(documents=docs_df, labels=label_df, tokenizer=token_gen.tokenize, bag_file='extracted-bags/extracted-bags-count-90.pickle')
     
-    bags = create_word_bags(docs_df, label_df, np.unique(label_df.columns), 60, custom_tokenizer=token_gen.tokenize)
+    # extractor.create_word_bags(90)
+    # extractor.save_word_bags()
+    
+    print(extractor.feature_count_class_words().head(20), '\n')
+    print(extractor.feature_doc_len().head(20), '\n')
+    print(extractor.feature_avg_word_len().head(20), '\n')
+    print(extractor.feature_fraction_upper().head(20), '\n')
+    print(extractor.feature_fraction_lower().head(20), '\n')
+    print(extractor.feature_fraction_titled().head(20), '\n')
 
-    #extractor_out = open('Extraction.out', mode='w')
+    
 
-    for bag in bags:
-        print('Label:', bag, '\n', bags[bag], end='\n\n')
+    
         
--- TweetReader.py
+++ TweetReader.py
@@ -12,22 +12,37 @@ if strip_mentions is true, will remove @ mentions from tweets before adding it
 
 returns the tweets as a pandas data frame
 '''
-labels = ['anger', 'anticipation', 'disgust', 'fear', 'joy', 'love', 'optimism', 'pessimism', 'sadness', 'surprise', 'trust']
+class TweetReader:
+    labels = ['anger', 'anticipation', 'disgust', 'fear', 'joy', 'love', 'optimism', 'pessimism', 'sadness', 'surprise', 'trust']
+        
+    def read_tweets(self, file, strip_mentions=True, sep_char=',', tweet_column_name='Tweet'):
+        self.data = pd.read_csv(file, sep=sep_char, encoding='utf8').drop('ID', axis=1)
+        
+        if strip_mentions:
+            self.data[tweet_column_name] = self.data[tweet_column_name].replace(to_replace='(@\S*)', value='', regex = True)
+            self.data = self.data.replace({0: False, 1: True})
+            #pd.options.display.encoding = 'unicode'
 
-def read_tweets(file, strip_mentions=True, sep_char=',', tweet_column_name='Tweet'):
-    data = pd.read_csv(file, sep=sep_char, encoding='utf8')
-    if strip_mentions:
-        data[tweet_column_name] = data[tweet_column_name].replace(to_replace='(@\S*)', value='', regex = True)
-        data = data.replace({0: False, 1: True})
-        #pd.options.display.encoding = 'unicode'
+        data_df = self.data[tweet_column_name]
+        label_df = self.data[self.labels]
 
-    data_df = data[tweet_column_name]
-    label_df = data[labels]
+        return data_df, label_df
 
-    return data_df, label_df
+    def save_tweets(self, path):
+        self.data.to_pickle(path)
+
+    def load_tweets(self, path, tweet_column_name='Tweet'):
+        self.data = pd.read_pickle(path)
+
+        data_df = self.data[tweet_column_name]
+        label_df = self.data[self.labels]
+
+        return data_df, label_df
+
     
 if __name__ == '__main__':
-    read_tweets('2018-E-c-En-train.txt', sep_char='\t')
+    reader = TweetReader()
+    reader.read_tweets('2018-E-c-En-train.txt', sep_char='\t')
 
         
         
